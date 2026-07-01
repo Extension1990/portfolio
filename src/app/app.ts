90faa6bea755b413ca56fd99c5af1b819de32890
Extension1990 <extensiontlhareseng@gmail.com>
2026-07-01 16:12:59 +0200

    Navbar done!

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgToastComponent } from 'ng-angular-popup';
+import { Navbar } from './core/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgToastComponent],
+  imports: [RouterOutlet, NgToastComponent, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
