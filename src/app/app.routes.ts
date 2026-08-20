d8c4a8af64ffce48f8dc14e8088dc733fda85af6
Extension1990 <extensiontlhareseng@gmail.com>
2026-08-20 12:34:23 +0200

    Added logo and fav icon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,16 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./pages/layout/layout').then((m) => m.Layout),
   },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard').then((m) => m.Dashboard),
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile').then((m) => m.Profile),
+  },
+  {
+    path: 'projects',
+    loadComponent: () => import('./pages/projects/projects').then((m) => m.Projects),
+  },
 ];
--- src/app/core/components/navbar/navbar.ts
+++ src/app/core/components/navbar/navbar.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-navbar',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, NgOptimizedImage, RouterLink],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
